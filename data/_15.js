5793715249e857bdf3a601c4d021e5430114e696
Vikitoriya622 <nedosekina.vicktoria@yandex.ru>
2026-02-10 23:02:23 +0700

    Add files via upload

---
+++ data/_15.js
@@ -0,0 +1,12 @@
+var json__15 = {
+"type": "FeatureCollection",
+"name": "_15",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "name": "СШОР по восточным единоборствам", "sport": null, "adress": "ул. Киевская 7", "region": "Ленинский район" }, "geometry": { "type": "Point", "coordinates": [ 82.849470929422381, 54.990112044819142 ] } },
+{ "type": "Feature", "properties": { "id": "1", "name": "ГАУ ДО НСО Спортивная школа по Каратэ", "sport": "каратэ", "adress": "ул. Петухова 101/2", "region": "Кировский район" }, "geometry": { "type": "Point", "coordinates": [ 82.953704599647537, 54.942354346877487 ] } },
+{ "type": "Feature", "properties": { "id": "1", "name": "Новосибирская городская федерация каратэ кекусинкай", "sport": "каратэ кекусинкай", "adress": "ул. Чигорина 16", "region": "Кировский район" }, "geometry": { "type": "Point", "coordinates": [ 82.958904951615196, 54.958954091887207 ] } },
+{ "type": "Feature", "properties": { "id": "1", "name": "ДО НСО СШОР по биатлону", "sport": "лыжный спорт", "adress": "ул. Биатлонная 1", "region": "Дзержинский район" }, "geometry": { "type": "Point", "coordinates": [ 83.041494457302932, 55.05456887969963 ] } },
+{ "type": "Feature", "properties": { "id": "1", "name": "СШОР ЦИВС", "sport": "волейбол", "adress": "ул. Учительская 61", "region": "Калининский район" }, "geometry": { "type": "Point", "coordinates": [ 82.952661512821905, 55.089876226551169 ] } }
+]
+}
---
+++ data/_35.js
@@ -0,0 +1,8 @@
+var json__35 = {
+"type": "FeatureCollection",
+"name": "_35",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "27", "name": "СДЮШОР по водным видом спортам", "sport": "парусный спорт", "adress": "ул. Лазурная 10/3", "region": "Октябрьский район", "filial": null, "metro": null, "Ж/К": "Восточный МЖК ж/м" }, "geometry": { "type": "Point", "coordinates": [ 83.012637139703202, 55.032251267128657 ] } }
+]
+}
